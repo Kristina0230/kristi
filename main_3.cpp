bc25d4b1f7d0911670d4cc951569fd9bdb4b1b60
Kristina0230 <125906479+Kristina0230@users.noreply.github.com>
2023-05-07 19:28:11 +0300

    main3.cpp

--- main_3.cpp
+++ main_3.cpp
@@ -1,93 +1,121 @@
+//lab3
 #include <iostream>
 #include <fstream>
 #include <stack>
 #include <string>
+#include <sstream>
+
+
+using namespace std;
 
 struct Student {
-    std::string name;
-    std::string surname;
-    int age;
-    float gpa;
-    std::string id;
+    string name;
+    string surname;
+    string faculty;
+    int birth_year;
+    int graduation_year;
+    int id;
 };
 
-// Функция для вывода информации о студенте на экран
-void printStudent(const Student& s) {
-    std::cout << s.name << " " << s.surname << ", " << s.age << " years old, "
-              << "GPA: " << s.gpa << ", ID: " << s.id << std::endl;
+
+Student read_student(const string& line) {
+    Student s;
+    istringstream iss(line);
+    iss >> s.name >> s.surname >> s.faculty >> s.birth_year >> s.graduation_year >> s.id;
+    return s;
+}
+
+
+
+void print_student(const Student& s) {
+    cout << "Name: " << s.name << endl;
+     cout << "Surname: " << s.surname << endl;
+    cout << "Faculty: " << s.faculty << endl;
+    cout << "Birth year: " << s.birth_year << endl;
+    cout << "Graduation year: " << s.graduation_year << endl;
+    cout << "ID: " << s.id << endl;
+    cout << endl;
+}
+
+
+void read_students(stack<Student>& students, const string& filename) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            students.push(read_student(line));
+        }
+        file.close();
+    }
+    else {
+        cout << "Невозможно открыть файл" << endl;
+    }
+}
+
+
+void print_students(const stack<Student>& students) {
+    stack<Student> copy = students;
+    while (!copy.empty()) {
+        print_student(copy.top());
+        copy.pop();
+    }
+}
+
+Student* find_student(stack<Student>& students, int id) {
+    stack<Student> copy = students;
+    while (!copy.empty()) {
+        if (copy.top().id == id) {
+            return &copy.top();
+        }
+        copy.pop();
+    }
+    return nullptr;
+}
+
+void remove_student(stack<Student>& students, int id) {
+    stack<Student> temp;
+    while (!students.empty()) {
+        if (students.top().id != id) {
+            temp.push(students.top());
+        }
+        students.pop();
+    }
+    while (!temp.empty()) {
+        students.push(temp.top());
+        temp.pop();
+    }
+}
+
+
+void add_student(stack<Student>& students, const Student& new_student) {
+    students.push(new_student);
 }
 
 int main() {
-    // Открываем файл с данными студентов
-    std::ifstream fin("students.txt");
+    stack<Student> students;
 
-    // Создаем пустой стек студентов
-    std::stack<Student> students;
+    read_students(students, "students.txt");
 
-    // Читаем данные из файла и добавляем их в стек
-    std::string name, surname, id;
-    int age;
-    float gpa;
-    while (fin >> name >> surname >> age >> gpa >> id) {
-        Student s = {name, surname, age, gpa, id};
-        students.push(s);
-    }
+    print_students(students);
 
-    // Выводим информацию о каждом студенте в стеке
-    std::cout << "Students in the stack:" << std::endl;
-    while (!students.empty()) {
-        Student s = students.top();
-        printStudent(s);
-        students.pop();
-    }
-
-    // Добавляем нового студента в стек
-    Student newStudent = {"John", "Doe", 20, 3.5, "ID12345"};
-    students.push(newStudent);
-
-    // Ищем студента по ID
-    std::string searchID = "ID16619";
-    std::stack<Student> tempStack; // временный стек для хранения студентов
-    bool found = false;
-    while (!students.empty()) {
-        Student s = students.top();
-        students.pop();
-        if (s.id == searchID) {
-            std::cout << "Student found:" << std::endl;
-            printStudent(s);
-            found = true;
-            break;
-        }
-        else {
-            tempStack.push(s);
-        }
-    }
-    // Возвращаем студентов из временного стека в исходный стек
-    while (!tempStack.empty()) {
-        students.push(tempStack.top());
-        tempStack.pop();
-    }
-    if (!found) {
-        std::cout << "Student with ID " << searchID << " not found." << std::endl;
-    }
-
-    // Удаляем студента с заданным ID
-    std::string deleteID = "ID12777";
-    found = false;
-    while (!students.empty()) {
-        Student s = students.top();
-        students.pop();
-        if (s.id == deleteID) {
-            std::cout << "Student deleted:" << std::endl;
-            printStudent(s);
-            found = true;
-            break;
-        }
-    }
-    if (!found) {
-        std::cout << "Student with ID " << deleteID << " not found." << std::endl;
-    }
-
-    return 0;
+int id = 1;
+Student* s = find_student(students, id);
+if (s != nullptr) {
+    cout << "Студент с ID" << id << " найден:" << endl;
+    print_student(*s);
+}
+else {
+    cout << "Студент с ID " << id << " не найден " << endl;
 }
 
+int id_to_remove = 4;
+remove_student(students, id_to_remove);
+cout << "Студент с ID " << id_to_remove << " удален" << endl;
+
+Student new_student = {"John", "Doe", "Computer Science", 1994, 2026, 51};
+add_student(students, new_student);
+cout << "Добавлен новый студент:" << endl;
+print_student(students.top());
+
+return 0;
+}
